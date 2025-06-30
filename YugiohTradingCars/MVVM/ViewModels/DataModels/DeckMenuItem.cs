e31876279db75a7063fcf0e03303d3b638d46ad1
schuetzler <schuetzler@virtual-office.de>
2025-06-30 11:05:58 +0200

    Doku: DeckMenuItem dokumentiert

--- YugiohTradingCars/MVVM/ViewModels/DataModels/DeckMenuItem.cs
+++ YugiohTradingCars/MVVM/ViewModels/DataModels/DeckMenuItem.cs
@@ -1,13 +1,26 @@
-﻿using System.Collections.Specialized;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 using YugiohTradingCars.DataModels;
 
 namespace YugiohTradingCars.MVVM.ViewModels.DataModels
 {
+    /// <summary>
+    /// Dieses Objekt stellt ein Wrapper für die Ui dar , nämlich eines MenuItem's
+    /// </summary>
     public class DeckMenuItem
     {
+        /// <summary>
+        /// Anzeige Name des Decks
+        /// </summary>
         public string DisplayText { get; set; }
+
+        /// <summary>
+        /// Dies soll als Command zum hinzufügen zum deck genutzt werden
+        /// </summary>
         public ICommand Command { get; set; }
+
+        /// <summary>
+        /// Objekt der des Decks, worin die Karten enthalten sind
+        /// </summary>
         public Deck Deck { get; set; }
     }
 }
